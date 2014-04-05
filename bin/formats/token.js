844281b43df82d558243c335424e72315f4c5584
Ioan CHIRIAC <i.chiriac@yahoo.com>
2014-04-05 20:45:20 +0200

    start to test tokens

--- bin/formats/token.js
+++ bin/formats/token.js
@@ -4,11 +4,22 @@
  * @author Ioan CHIRIAC
  * @license BSD-3-Clause
  */
-
+var fs = require('fs');
+var exec = require('child_process').exec;
 module.exports = {
   handles: function(filename, ext) {
     return filename.substring(0, 9) == '../token/';
   }
   ,run: function(filename, PHP) {
+    var jsTok = PHP.globals.__call(
+      'token_get_all',
+      fs.readFileSync(filename).toString()
+    );
+    exec('php ' + __dirname + '/token.php ' + filename, function(error, stdout, stderr) {
+      var phpTok = JSON.parse(stdout);
+      console.log('JS Output ', jsTok);
+      console.log('PHP Output ', phpTok);
+    });
+    return true;
   }
 };
